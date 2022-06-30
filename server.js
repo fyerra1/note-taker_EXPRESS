5c79c52d427fe2ef4eee2f0b9ed1431d0d8df576
Franky Erra <frankyerra43@gmail.com>
2022-06-29 21:03:50 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const html = require('./routes/htmlroutes');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+app.use(html);
+
+app.listen(PORT, () => {
+  console.log(`listening on port http://localhost:${PORT}/`);
+})
+
