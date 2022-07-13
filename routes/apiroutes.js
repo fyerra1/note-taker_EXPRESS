54a4ecb97bfc180139df672801cd7b1f0405c4c2
Franky Erra <frankyerra43@gmail.com>
2022-07-13 00:47:34 -0700

    removed commented code

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,7 +5,6 @@ const { v4: uuidv4 } = require('uuid');
 
 
 router.get('/notes', (req, res) => {
-  console.info(`${req.method} request received to get notes`);
   res.sendFile(path.join(__dirname, '../db/db.json'));  
 });
 
@@ -26,55 +25,7 @@ router.post('/notes', (req, res) => {
           res.json(parsedData);    
       }
   })
-  
 })
 
 
 module.exports = router;
-
-// fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-// err ? console.error(err) : console.info(`\nData written to ${destination}`)
-// );
-
-// tips.get('/', (req, res) => {
-//   readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
-// });
-
-// fb.post('/', (req, res) => {
-//   // Destructuring assignment for the items in req.body
-//   const { email, feedbackType, feedback } = req.body;
-
-//   // If all the required properties are present
-//   if (email && feedbackType && feedback) {
-//     // Variable for the object we will save
-//     const newFeedback = {
-//       email,
-//       feedbackType,
-//       feedback,
-//       feedback_id: uuidv4(),
-//     };
-
-//     readAndAppend(newFeedback, './db/feedback.json');
-
-//     const response = {
-//       status: 'success',
-//       body: newFeedback,
-//     };
-
-//     res.json(response);
-//   } else {
-//     res.json('Error in posting feedback');
-//   }
-// });
-
-// const readAndAppend = (content, file) => {
-//   fs.readFile(file, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//     } else {
-//       const parsedData = JSON.parse(data);
-//       parsedData.push(content);
-//       writeToFile(file, parsedData);
-//     }
-//   });
-// };
