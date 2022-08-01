d6023e56bdd1d12f947cabbb68aec322c4dea695
Franky Erra <frankyerra43@gmail.com>
2022-07-31 18:17:58 -0700

    modified port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const html = require('./routes/htmlroutes');
 const api = require('./routes/apiroutes');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
